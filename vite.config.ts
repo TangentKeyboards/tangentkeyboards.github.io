b43b9a9708b505cf84111f307a571e8740c20fbd
Mafrans <malte.kluft@gmail.com>
2023-05-22 15:14:45 +0200

    feat: add basic markdown cms

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
+import { plugin as markdown, Mode } from "vite-plugin-markdown";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [markdown({ mode: [Mode.HTML] }), sveltekit()],
 });
