0257c916818ae9cc2f7580ab610ce66cee3fae95
Mafrans <malte.kluft@gmail.com>
2023-05-22 12:02:12 +0200

    fix: config.kit.target is no longer required

--- svelte.config.js
+++ svelte.config.js
@@ -6,11 +6,7 @@ const config = {
   preprocess: vitePreprocess(),
 
   kit: {
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: "#svelte",
-
     adapter: adapter({
-      // default options are shown
       pages: "build",
       assets: "build",
       fallback: null,
