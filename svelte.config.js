49fda05f51a99aa1657b40fcc88697a35f2f1588
Mafrans <malte.kluft@gmail.com>
2023-05-22 11:58:10 +0200

    fix: update build and deploy script according to docs

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,15 @@ const config = {
   preprocess: vitePreprocess(),
 
   kit: {
-    adapter: adapter(),
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: "#svelte",
+
+    adapter: adapter({
+      // default options are shown
+      pages: "build",
+      assets: "build",
+      fallback: null,
+    }),
   },
 };
 
