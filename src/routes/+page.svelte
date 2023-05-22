8ad446450c4fa8d26453d0f077f7fca2662000e5
Mafrans <malte.kluft@gmail.com>
2023-05-22 11:39:56 +0200

    feat: add wip page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Tangent Keyboards</h1>
+<p>This website is under construction.</p>
+<p>Contact <a href="https://mafra.nz">Malte</a> if you have any questions.</p>
